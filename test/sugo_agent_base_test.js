928f1bc68ff076dfa7b8585fc97343196896094b
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-27 15:38:27 +0900

    Add test for agent

--- test/sugo_agent_base_test.js
+++ test/sugo_agent_base_test.js
@@ -6,23 +6,36 @@
 
 const SugoAgentBase = require('../lib/sugo_agent_base.js')
 const assert = require('assert')
+const sgServer = require('sg-server')
+const { freeport } = sgServer
 const co = require('co')
 
 describe('sugo-agent-base', function () {
   this.timeout(3000)
-
+  let server, port
   before(() => co(function * () {
+    port = yield freeport()
+    server = sgServer({
+      endpoints: {
+        '/api/foo': (ctx) => {
+          ctx.set('foo', 'This is foo')
+          ctx.body = null
+        }
+      }
+    })
 
+    yield server.listen(port)
   }))
 
   after(() => co(function * () {
-
+    yield server.close()
   }))
 
   it('Sugo agent base', () => co(function * () {
-    let agent = new SugoAgentBase('http://example.com')
+    let agent = new SugoAgentBase(`http://localhost:${port}`)
     assert.ok(agent)
-    assert.equal(agent.url, 'http://example.com')
+    let ok = yield agent.knock('/api/foo')
+    assert.ok(ok)
   }))
 })
 
